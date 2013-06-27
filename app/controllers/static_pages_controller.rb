c267ddd7af7ff5565ebc4d4cfacda75d7647471c
Franky W. (franklinwahl - Frankys-MacBook-Pro.local) <wahl77+git@gmail.com>
2013-06-27 09:07:13 -0700

    First version

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,24 @@
+class StaticPagesController < ApplicationController
+  def index
+    @students = [
+    "Alex Lin          ",
+    "Neal Barbaro      ",
+    "Adam Daigiane    ",
+    "Elizabeth Hyde    ",
+    "Amy Wing Erz      ",
+    "Eric Mooney      ",
+    "Yekaterina        ",
+    "Jason Thein      ",
+    "Claire Pierce    ",
+    "Patsy Price      ",
+    "Isaac Elias      ",
+    "Zara Peisker      ",
+    "Franky            ",
+    "Christina Stewart ",
+    "Baylee Feore      ",
+    "Harrison Tan      ",
+    "Thomas Galpin    ",
+    "Gabriel Dominguez "     
+    ] 
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheel::Application.config.secret_token = '81d5e80dbddb69cbd43f39c1d381688032b5faf151f5162240fc27390ccc47821f274b4794e766043c33c6cd43d2311d9f46d591fc66abd6c318a337f0d8f20a'
